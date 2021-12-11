0ee37f3ae3f88640b060d52803e28251ce33986f
sivaganga75 <95561631+sivaganga75@users.noreply.github.com>
2021-12-11 03:58:20 -0800

    update java file

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ package SimpleJava;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to JAVA");
+		System.out.println("Welcome to JAVA training");
 
 	}
 
